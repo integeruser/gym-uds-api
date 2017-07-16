c711fb2a077eb8a2d04c40c981748cd9fe9c74f3
Francesco Cagnin <francesco.cagnin@gmail.com>
2017-07-16 15:16:55 +0200

    Cleanup

--- gym-server.py
+++ gym-server.py
@@ -7,23 +7,8 @@ import gym
 import gym_pb2
 
 
-def run(sock, conn):
-    env = gym.make('CartPole-v0')
-
-    observation = env.reset()
-    send_state(observation, 0.0, False)
-
-    done = False
-    while not done:
-        action = recv_action()
-        observation, reward, done, _ = env.step(action)
-        send_state(observation, float(reward), bool(done))
-
-
 def recv_action():
-    action_pb_len = int.from_bytes(conn.recv(1), byteorder='little')
-    action_pb = conn.recv(action_pb_len)
-
+    action_pb = conn.recv(int.from_bytes(conn.recv(1), byteorder='little'))
     action = gym_pb2.Action()
     action.ParseFromString(action_pb)
     return action.value
@@ -31,18 +16,28 @@ def recv_action():
 
 def send_state(observation, reward, done):
     state_pb = gym_pb2.State(value=observation, reward=reward, done=done).SerializeToString()
-    conn.sendall(len(state_pb).to_bytes(1, byteorder='little'))
-    conn.sendall(state_pb)
+    conn.sendall(len(state_pb).to_bytes(1, byteorder='little') + state_pb)
 
 
 if __name__ == '__main__':
     socket_filepath = '/tmp/gym-server-socket'
+    try:
+        os.remove(socket_filepath)
+    except FileNotFoundError:
+        pass
 
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     sock.bind(socket_filepath)
-    sock.listen(0)
+    sock.listen()
 
-    conn, _ = sock.accept()
-    run(sock, conn)
+    env = gym.make('CartPole-v0')
+    while True:
+        conn, _ = sock.accept()
+        observation = env.reset()
+        send_state(observation, 0.0, False)
 
-    os.remove(socket_filepath)
+        done = False
+        while not done:
+            action = recv_action()
+            observation, reward, done, _ = env.step(action)
+            send_state(observation, float(reward), bool(done))
--- gym-test-client.py
+++ gym-test-client.py
@@ -5,9 +5,7 @@ import gym_pb2
 
 
 def recv_state():
-    state_pb_len = int.from_bytes(sock.recv(1), byteorder='little')
-    state_pb = sock.recv(state_pb_len)
-
+    state_pb = sock.recv(int.from_bytes(sock.recv(1), byteorder='little'))
     state = gym_pb2.State()
     state.ParseFromString(state_pb)
     return state.value, state.reward, state.done
@@ -15,8 +13,7 @@ def recv_state():
 
 def send_action(action):
     action_pb = gym_pb2.Action(value=action).SerializeToString()
-    sock.sendall(len(action_pb).to_bytes(1, byteorder='little'))
-    sock.sendall(action_pb)
+    sock.sendall(len(action_pb).to_bytes(1, byteorder='little') + action_pb)
 
 
 if __name__ == '__main__':

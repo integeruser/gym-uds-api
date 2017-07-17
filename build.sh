7cbde8d129dfd2ee13fb121fe5642a69d4b14615
Francesco Cagnin <francesco.cagnin@gmail.com>
2017-07-17 15:35:33 +0200

    Made build.sh execute a user specified protoc

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-protoc --python_out=. gym.proto
-protoc --cpp_out=binding-cpp gym.proto
-mv binding-cpp/gym.pb.h binding-cpp/include/gym.pb.h
-mv binding-cpp/gym.pb.cc binding-cpp/src/gym.pb.cc
+if [[ ! -z $1 ]] ; then
+    PROTOC=$1
+else
+    PROTOC=$(which protoc)
+fi
+
+if [[ ! -z $PROTOC ]] ; then
+    $PROTOC --python_out=. gym.proto &&
+    $PROTOC --cpp_out=binding-cpp gym.proto &&
+    mv binding-cpp/gym.pb.h binding-cpp/include/gym.pb.h &&
+    mv binding-cpp/gym.pb.cc binding-cpp/src/gym.pb.cc
+fi
